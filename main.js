9b2fe2450a0fd9a34898c4eeffbd3661df130e26
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-10-30 16:03:08 -0500

    Lesson 8 Computed Properties

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'These are comfy leggings for your feet!',
-            image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
             url: 'https://johnscrazysocks.com',
-            inventory: 100,
-            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: false },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: true },
             ],
-            sizes: ['S', 'M', 'L', 'XL']
+            sizes: ['S', 'M', 'L', 'XL'],
+            activeClass: true
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].onSale
         }
     }
 })
